d763d3786c3891957d28a0eb28e61f172fa32d83
NikitaPilez <pileznikita99@gmail.com>
2023-03-13 12:45:30 +0300

    Product review

---
+++ database/migrations/2023_03_13_093636_create_product_reviews_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('product_reviews', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('product_id');
+            $table->text('text');
+            $table->unsignedBigInteger('user_id')->nullable();
+            $table->boolean('is_active')->nullable()->default(1);
+            $table->timestamps();
+
+            $table->foreign('product_id')->references('id')->on('products')->cascadeOnDelete();
+            $table->foreign('user_id')->references('id')->on('users')->nullOnDelete();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('product_reviews');
+    }
+};
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,7 +15,8 @@ class DatabaseSeeder extends Seeder
         $this->call([
             CategoriesTableSeeder::class,
             ProductsTableSeeder::class,
-            ProductImagesSeeder::class
+            ProductImagesSeeder::class,
+            ProductReviewsTableSeeder::class
         ]);
     }
 }
